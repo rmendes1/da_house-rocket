80a94b00222d6ef0f08aaa4fce38d73961bcdc38
rmendes1 <joaorenatomendes@gmail.com>
2021-11-18 11:20:22 -0300

    map and function 'average_metrics' have been removed due to errors (they will be fixed later)

--- dashboard.py
+++ dashboard.py
@@ -31,7 +31,6 @@ def get_geofile(path): #change to url
 
 def data_overview(data):
     st.title('House Rocket Exploratory Data Analysis')
-    st.header('Dataset Overview')
     f_attributes = st.sidebar.multiselect('Enter columns', data.columns)
     f_zipcode = st.sidebar.multiselect('Enter zipcode', sorted(set(data['zipcode'].unique())))
     st.title('Data Overview')
@@ -56,101 +55,7 @@ def data_overview(data):
 
     return None
 
-def average_metrics(data):
 
-    c1, c2 = st.columns((1, 1))
-
-    # Average metrics
-    df1 = data[['id', 'zipcode']].groupby('zipcode').count().reset_index()
-    df2 = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
-    df3 = data[['sqft_living', 'zipcode']].groupby('zipcode').count().reset_index()
-    df4 = data[['price_m2', 'zipcode']].groupby('zipcode').mean().reset_index()
-
-    # merge
-    m1 = pd.merge(df1, df2, on='zipcode', how='inner')
-    m2 = pd.merge(m1, df3, on='zipcode', how='inner')
-    df = pd.merge(m2, df4, on='zipcode', how='inner')
-
-    # st.write(df.head())
-
-    df.columns = ['ZIPCODE', 'TOTAL HOUSES', 'PRICE', 'SQFT LIVING', 'PRICE/M2']
-
-    c1.header('Average Values')
-    c1.dataframe(df, height=600)
-
-    # Descriptive Statistics 01
-    num_attributes = data.select_dtypes(include=['int64', 'float64'])
-
-    media = pd.DataFrame(num_attributes.apply(np.mean))
-    mediana = pd.DataFrame(num_attributes.apply(np.median))
-    std = pd.DataFrame(num_attributes.apply(np.std))
-    max_ = pd.DataFrame(num_attributes.apply(np.max))
-    min_ = pd.DataFrame(num_attributes.apply(np.min))
-
-    df1 = pd.concat([max_, min_, media, mediana, std], axis=1).reset_index()
-
-    df1.columns = ['attributes', 'max', 'min', 'mean', 'median', 'std']
-
-    c2.header('Descriptive Analysis')
-    c2.dataframe(df1, height=600)
-
-    return None
-
-def portfolio_density(data, geofile):
-    st.title('Region Overview')
-    c1, c2 = st.columns((1, 1))
-    c1.header('Portfolio Density')
-
-    df = data.sample(10)
-
-    # -----------
-    # MAP CONSTRUCTION - folium
-    # --------------
-
-    density_map = folium.Map(location=[data['lat'].mean(), data['long'].mean()],
-                             default_zoom_start=15)
-
-    # st.write(density_map)
-    marker_cluster = MarkerCluster().add_to(density_map)
-
-    for name, row in df.iterrows():
-        folium.Marker(([row['lat'], row['long']]),
-                      popup='Price {0}, Sold on: {1}, with sqft: {2} m2'.format(row['price'],
-                                                                                row['date'],
-                                                                                row['sqft_living'])).add_to(
-            marker_cluster)
-
-    with c1:
-        folium_static(density_map)
-
-    # -------
-    # Region Price Map
-    # -------
-
-    c2.header("Price Density")
-
-    df = data[['price', 'zipcode']].groupby('zipcode').mean().reset_index()
-
-    df.columns = ['ZIP', 'PRICE']
-    df.sample(10)
-    geofile = geofile[geofile['ZIP'].isin(df['ZIP'].tolist())]
-
-    region_price_map = folium.Map(location=[data['lat'].mean(), data['long'].mean()],
-                                  default_zoom_start=15)
-
-    region_price_map.choropleth(data=df,
-                                geo_data=geofile,
-                                columns=['ZIP', 'PRICE'],
-                                key_on='feature.properties.ZIP',
-                                fill_color='YlOrRd',
-                                fill_opacity=0.7,
-                                line_opacity=0.2,
-                                legend_name='AVG PRICE')
-
-    with c2:
-        folium_static(region_price_map)
-
-    return None
 ### What are the estates that House Rocket should buy and for how much? ###
 ### COLOCAR MAPA DE DENSIDADE EM CIMA OU EMBAIXO
 def buy_estates(data):
@@ -191,11 +96,8 @@ def buy_estates(data):
 
     c1.plotly_chart(fig, use_container_width=True)  #creates a minor plot with a description of how many estates HR should buy
 
-    return None
 
 #----SET SEASONS ON DATAFRAME----#
-
-def estates_season(data):
     st.header('2. What is/are the best season(s) to buy a estate?')
     c1, c2 = st.beta_columns(2)
     c1.subheader('Estates per region x Sales Percentual')
@@ -258,7 +160,4 @@ if __name__ == '__main__':
 
     # ---- Transformation
     data_overview(data)
-    average_metrics(data)
-    portfolio_density(data, geofile)
     buy_estates(data)
-    estates_season(data)
